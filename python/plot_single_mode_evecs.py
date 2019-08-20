c127f1eafb40e0fb99369794588f8a3e1f10d64f
J. S. Oishi <jsoishi@gmail.com>
2019-08-20 16:34:44 -0400

    Plot 2D x-y plane visualizations of eigenmodes as well as 1D

--- python/plot_single_mode_evecs.py
+++ python/plot_single_mode_evecs.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import h5py
 import sys
+import numpy as np
 from pathlib import Path
 
 plt.style.use('prl')
@@ -9,6 +10,7 @@ filename = Path(sys.argv[-1])
 rows = 2
 cols = 3
 names = [r'$v_x$',r'$v_y$',r'$v_z$',r'$b_x$',r'$b_y$',r'$b_z$']
+cmaps = ['RdBu','RdBu','RdBu','PuOr','PuOr','PuOr']
 plt.figure(figsize=(12,6))
 with h5py.File(filename, 'r') as df:
     x = df['eigvecs'].attrs['x']
@@ -21,7 +23,34 @@ with h5py.File(filename, 'r') as df:
         plt.xlabel(r'$x$')
         plt.ylabel(names[i-1])
 
-plt.tight_layout()
+    plt.tight_layout()
+    plt.savefig('plots/' + filename.stem + 'eigvecs.pdf')
 
-plt.savefig('plots/' + filename.stem + 'eigvecs.pdf')
+    # 2D plots
+    n_yz = 128
+    ky = df['eigvals'].attrs['ky']
+    kz = df['eigvals'].attrs['kz']
+    Ly = 2*np.pi/ky
+    Lz = 2*np.pi/kz
 
+    y = np.linspace(0, Ly, n_yz, endpoint=False)
+
+    yyy, xxx = np.meshgrid(y,x,indexing='ij')
+    y_dep = np.exp(1j*ky*yyy)
+    plt.figure(figsize=(10,10))
+    eigvec = df['eigvecs']
+    for i in range(1,7):
+        ax = plt.subplot(rows,cols,i)
+        pcol = plt.pcolormesh(xxx,yyy,(eigvec[i,:]*y_dep).real,linewidth=0,rasterized=True, cmap=cmaps[i-1])
+        #pcol.set_edgecolor('face')
+        plt.title(names[i-1])
+        if i !=4:
+            ax.axes.get_xaxis().set_visible(False)
+            ax.axes.get_yaxis().set_visible(False)
+        else:
+            plt.xlabel(r'$x$')
+            plt.ylabel(r'$y$')
+            ax.set_xticklabels(["$-\pi/2$", 0, "$\pi/2$"])
+            ax.set_xticks([-np.pi/2, 0, np.pi/2])
+
+    plt.savefig('plots/eigvecs_xy_' + filename.stem +'.pdf')
